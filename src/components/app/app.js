d60071dd63c82ffe3b8f69228448e70f4cd0fa2c
Veselin <vesselin60@gmail.com>
2021-12-03 09:58:49 +0200

    homework

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,20 +3,19 @@ import { Component } from 'react';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
-import EmployeesList from '../eployees-list/employees-list';
+import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
-
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             data: [
-                {name: 'Gocho', salary: 50000, increase: true, id: 1},
-                {name: 'Docho', salary: 800, increase: false, id: 2},
-                {name: 'Bocho', salary: 1000, increase: false, id: 3},
+                {name: 'John C.', salary: 800, increase: false, rise: true, id: 1},
+                {name: 'Alex M.', salary: 3000, increase: true, rise: false, id: 2},
+                {name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3}
             ]
         }
         this.maxId = 4;
@@ -24,29 +23,19 @@ class App extends Component {
 
     deleteItem = (id) => {
         this.setState(({data}) => {
-            // const index = data.findIndex(elem => elem.id === id);
-
-            // const before = data.slice(0, index);
-            // const after = data.slice(index + 1);
-
-            // const newArr = [...before, ...after];
-
-            // return {
-            //     data: newArr
-            // }
-
             return {
                 data: data.filter(item => item.id !== id)
             }
-            
         })
     }
 
+    // Да, пока могут добавляться пустые пользователи. Мы это еще исправим
     addItem = (name, salary) => {
         const newItem = {
             name, 
-            salary, 
-            increase: false, 
+            salary,
+            increase: false,
+            rise: false,
             id: this.maxId++
         }
         this.setState(({data}) => {
@@ -57,23 +46,36 @@ class App extends Component {
         });
     }
 
+    onToggleProp = (id, prop) => {
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, [prop]: !item[prop]}
+                }
+                return item;
+            })
+        }))
+    }
+
     render() {
-        return(
+        const employees = this.state.data.length;
+        const increased = this.state.data.filter(item => item.increase).length;
+        return (
             <div className="app">
-                <AppInfo/>
-            
+                <AppInfo employees={employees} increased={increased}/>
+    
                 <div className="search-panel">
                     <SearchPanel/>
                     <AppFilter/>
                 </div>
-    
+                
                 <EmployeesList 
                     data={this.state.data}
                     onDelete={this.deleteItem}
-                />
+                    onToggleProp={this.onToggleProp}/>
                 <EmployeesAddForm onAdd={this.addItem}/>
             </div>
-         );
+        );
     }
 }
 
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,7 +1,8 @@
 import EmployeesListItem from "../employees-list-item/employees-list-item";
-import './employees-list.css'
 
-const EmployeesList = ({data, onDelete}) => {
+import './employees-list.css';
+
+const EmployeesList = ({data, onDelete, onToggleProp}) => {
 
     const elements = data.map(item => {
         const {id, ...itemProps} = item;
@@ -10,17 +11,15 @@ const EmployeesList = ({data, onDelete}) => {
                 key={id} 
                 {...itemProps}
                 onDelete={() => onDelete(id)}
-            /> //name={item.name} salary={item.salary}
-        );
-
-    });
+                onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}/>
+        )
+    })
 
     return (
-        <ul className="app-list list-group"> 
+        <ul className="app-list list-group">
             {elements}
         </ul>
-    );
-
+    )
 }
 
 export default EmployeesList;
