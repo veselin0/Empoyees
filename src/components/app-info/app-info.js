1cab5bb273460c600211f2b9f47c5348828b38c1
Veselin <vesselin60@gmail.com>
2021-12-03 11:55:12 +0200

    Prctice State Rise

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,11 @@
 import './app-info.css';
 
-const AppInfo = () => {
+const AppInfo = ({employees, increased}) => {
     return (
         <div className="app-info">
             <h1>Accounting of employees in the company Gocho</h1>
-            <h2>Total number of employees: </h2>
-            <h2>Awards will receive: </h2>
+            <h2>Total number of employees: {employees} </h2>
+            <h2>Awards will receive: {increased} </h2>
         </div>
     );
 }
